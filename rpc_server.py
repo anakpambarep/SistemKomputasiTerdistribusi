b8fe442ea02e9c66c08216546011419e99697f00
anakpambarep <erdiansahlan@gmail.com>
2018-03-15 21:01:27 +0700

    edit1

--- rpc_server.py
+++ rpc_server.py
@@ -23,7 +23,7 @@ def cek_prima(n):
     if (n == 1):
         return False
     elif (n == 2):
-        return True;
+        return True
     else:
         for x in range(2, n):
             if (n % x == 0):
